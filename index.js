45b02db3bb38c04f1e03aad94787e943c9f9f975
daniloczar <daniloczar@hotmail.com>
2023-03-20 20:03:31 +0000

    update js

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+/*Menu*/
+
 function clickMenu() {
     if(menu.style.display == "block"){
         menu.style.display = "none"
@@ -13,10 +15,58 @@ function changeMenu() {
         menu.style.display ="none"
     }
 }
-let time = document.getElementById("time");
- 
-setInterval(() => {
-    let d = new Date();
-    time.innerHTML = d.toLocaleTimeString();
- },1000)
 
+ // date and time
+
+ 
+ const timeElement = document.querySelector("#time");
+ const dateElement = document.querySelector("#date");
+ 
+ 
+ function formatTime(date) {
+   const hours12 = date.getHours() 
+   const minutes = date.getMinutes()
+   const seconds = date.getSeconds();
+ 
+   return `${hours12.toString().padStart(2, "0")}:${minutes.toString()
+    .padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
+ }
+ 
+ 
+ function formatDate(date) {
+   const DAYS = [
+     "Sunday",
+     "Monday",
+     "Tuesday",
+     "Wednesday",
+     "Thursday",
+     "Friday",
+     "Saturday"
+   ];
+   const MONTHS = [
+     "January",
+     "February",
+     "March",
+     "April",
+     "May",
+     "June",
+     "July",
+     "August",
+     "September",
+     "October",
+     "November",
+     "December"
+   ];
+ 
+   return `${DAYS[date.getDay()]}, ${
+     MONTHS[date.getMonth()]
+   } ${date.getDate()} ${date.getFullYear()}`;
+ }
+ 
+ setInterval(() => {
+   const now = new Date();
+ 
+   timeElement.textContent = formatTime(now);
+   dateElement.textContent = formatDate(now);
+ }, 1000);
+ 
